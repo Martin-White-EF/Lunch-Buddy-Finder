6888506e698b31ffccfed95d937bf74873e39b66
Martin-White-EF <martin.white@ef.com>
2025-02-19 11:13:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
+import random
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# List of coworkers
+coworkers = ['Laiyi', 'Karen', 'Svenja', 'Remi', 'Amanda', 'Penny', 'Mai', 'Teppei', 'Valentina', 'Saki', 'Satoru', 'Ryoko', 'Kiho', 'Yudai', 'Kei', 'Dan']
+
+# Initialize the lunchbuddy in the session state if not already set
+if 'lunchbuddy' not in st.session_state:
+    st.session_state.lunchbuddy = random.choice(coworkers) + '!!!!!'
+
+# Display the lunch buddy
+st.write(f"""
+# You are having lunch with! 
+## {st.session_state.lunchbuddy}
+""")
+
+# Button to reroll the lunchbuddy
+if st.button('Reroll'):
+    st.session_state.lunchbuddy = random.choice(coworkers) + '!!!!!'
+    #st.rerun()  # Reruns the entire script to update the displayed lunch buddy
